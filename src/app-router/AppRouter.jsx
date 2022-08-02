1985fe5ab5739a7a2c9600e3ffb7fd9208d11d25
yasin yağmur <yasin.yagmur@gmail.com>
2022-08-02 15:44:51 +0300

    route import and route tree created

--- src/app-router/AppRouter.jsx
+++ src/app-router/AppRouter.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Dashboard from "../pages/Dashboard";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Details from "../pages/Details";
+import NewBlog from "../pages/NewBlog";
 
 const AppRouter = () => {
-  return <>AppRouter</>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/details:id" element={<Details />} />
+          <Route path="/newblog" element={<NewBlog />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default AppRouter;
